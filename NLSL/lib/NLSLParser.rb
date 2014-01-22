206d047dd0268e683d948044c0a495b3b50349e2
32leaves <csweichel@googlemail.com>
2014-01-22 20:22:46 +0000

    Parser can be loaded from a statically compiled parser now

--- NLSL/lib/NLSLParser.rb
+++ NLSL/lib/NLSLParser.rb
@@ -3,7 +3,10 @@ require 'treetop'
 
 basepath = File.expand_path(File.dirname(__FILE__))
 require File.join(basepath, 'NLSL.rb')
-#require File.join(basepath, 'NLSLParserGen.rb')
-require File.join(basepath, 'class.treetopHelper.rb')
+if File.exists? File.join(basepath, 'NLSLParserGen.rb')
+  require File.join(basepath, 'NLSLParserGen.rb')
+else
+  Treetop.load(File.join(basepath, 'NLSLParser.treetop'))
+end
 
-Treetop.load(File.join(basepath, 'NLSLParser.treetop'))
+require File.join(basepath, 'class.treetopHelper.rb')
